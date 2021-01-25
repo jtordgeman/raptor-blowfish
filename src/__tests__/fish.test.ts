1f0eeefa919a98b3b20fea2d16002a11c7e123c2
Tropics <buserian@penguin.me>
2021-01-25 19:13:31 +0200

    refactor

--- src/__tests__/fish.test.ts
+++ src/__tests__/fish.test.ts
@@ -1,6 +1,6 @@
 import { Fish } from '../index';
 
 test('ecb encode', () => {
-    const fish = new Fish('testMyKey');
-    expect(fish.encrypt('hello world')).toBe('+OK vmyOX/qmteO/0DO15/KpSWX/');
+    const enc = Fish.createCipher('testMyKey');
+    expect(enc.encrypt('hello world')).toBe('+OK vmyOX/qmteO/0DO15/KpSWX/');
 });
