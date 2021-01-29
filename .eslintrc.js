8a683754f0d23236a2bf3d22647eddc82e637a7d
Tropics <buserian@penguin.me>
2021-01-29 13:33:36 +0200

    eslintrc lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,8 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2020,
-        sourceType: 'module'
+        sourceType: 'module',
     },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended'
-    ],
-    rules: {
-
-    }
-}
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+    rules: {},
+};
