3858d6916524789f64407082f4f9f23a419ef1ac
buserian <buha124a@yahoo.com>
2023-03-24 09:24:07 +0300

    cbc cipher fix

--- src/blowfishCipherCBC.ts
+++ src/blowfishCipherCBC.ts
@@ -6,11 +6,11 @@ export class BlowfishCipherCBC implements Cipher {
     constructor(private key: string) {}
 
     encrypt(input: string): string {
-        const paddedInput = utils.pad(input, 8);
+        const paddedInput = utils.padBuffer(Buffer.from(input, 'utf8'), 8);
         const IV = Buffer.from(input.slice(0, 8));
         const cipher = crypto.createCipheriv('bf-cbc', Buffer.from(this.key), IV);
         cipher.setAutoPadding(false);
-        const encrypted = Buffer.concat([IV, cipher.update(paddedInput, 'utf-8'), cipher.final()]).toString('base64');
+        const encrypted = Buffer.concat([cipher.update(paddedInput), cipher.final()]).toString('base64');
         return `+OK *${encrypted}`;
     }
 }
